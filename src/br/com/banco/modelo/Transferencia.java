47e706320d992a66040bd965cc23aefee862c82f
jose-ribeiro <williamribeiro419@gmail.com>
2020-10-23 20:11:33 -0400

    trans

--- src/br/com/banco/modelo/Transferencia.java
+++ src/br/com/banco/modelo/Transferencia.java
@@ -1,5 +1,14 @@
 package br.com.banco.modelo;
 
+import java.util.Date;
+
 public class Transferencia {
+	private Conta emissor;
+	private Conta destinatario;
+	private double valor;
+	private Date data;
+	
+	
+	
 
 }
